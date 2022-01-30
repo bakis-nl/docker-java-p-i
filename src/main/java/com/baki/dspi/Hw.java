b8d0f53da7aebd41308a3f15df80e90ab607fa8d
bakis-nl <baki.sevgul@gmail.com>
2022-01-30 06:55:48 +0100

    second commit

--- src/main/java/com/baki/dspi/Hw.java
+++ src/main/java/com/baki/dspi/Hw.java
@@ -2,6 +2,6 @@ package com.baki.dspi;
 
 public class Hw {
     public static void main(String[] args) {
-        System.out.println("baki was here !..");
+        System.out.println("baki was here !...");
     }
 }
